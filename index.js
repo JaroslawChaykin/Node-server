062702afe5d376d3648dd8161b422a49224e35b8
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-07-06 17:51:18 +0600

    get user data

--- index.js
+++ index.js
@@ -1,64 +1,95 @@
 import express from "express";
 import mongoose from "mongoose";
-import multer from "multer"
-import cors from "cors"
+import multer from "multer";
+import cors from "cors";
 
-import {loginValidation, postCreateValidation, registerValidation} from "./validations/validations.js";
-import {checkAuth, handleValidationErrors} from "./utils/index.js";
-import {UserController, PostController, CategoriesController} from "./controllers/index.js"
+import {
+    loginValidation,
+    postCreateValidation,
+    registerValidation,
+} from "./validations/validations.js";
+import { checkAuth, handleValidationErrors } from "./utils/index.js";
+import {
+    UserController,
+    PostController,
+    CategoriesController,
+} from "./controllers/index.js";
 
 mongoose
-  .connect(
-    "mongodb+srv://jaroslawchaikin:wwwww1@cluster0.1s3apa6.mongodb.net/blog"
-  )
-  .then(() => {
-    console.log("DB OK");
-  })
-  .catch((err) => console.log("DB ERROR", err));
+    .connect(
+        "mongodb+srv://jaroslawchaikin:wwwww1@cluster0.1s3apa6.mongodb.net/blog"
+    )
+    .then(() => {
+        console.log("DB OK");
+    })
+    .catch((err) => console.log("DB ERROR", err));
 
 const app = express();
 
 const storage = multer.diskStorage({
-  destination: (_, __, cb) => {
-    cb(null, 'uploads')
-  },
-  filename: (_, file, cb) => {
-    cb(null, file.originalname)
-  }
+    destination: (_, __, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
 });
 
-const upload = multer({storage})
+const upload = multer({ storage });
 
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
-app.use(cors())
+app.use("/uploads", express.static("uploads"));
+app.use(cors());
 
-app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
-app.post("/auth/register", registerValidation, handleValidationErrors, UserController.register);
+app.post(
+    "/auth/login",
+    loginValidation,
+    handleValidationErrors,
+    UserController.login
+);
+app.post(
+    "/auth/register",
+    registerValidation,
+    handleValidationErrors,
+    UserController.register
+);
 app.get("/auth/me", checkAuth, UserController.getMe);
 app.delete("/auth/remove", checkAuth, UserController.deleteMe);
+app.get("/user/:nickname", UserController.getUserData);
 
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-  console.log(req.file)
-  res.json({
-    url: `uploads/${req.file.originalname}`
-  })
-})
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+    console.log(req.file);
+    res.json({
+        url: `uploads/${req.file.originalname}`,
+    });
+});
 
-app.get('/posts', PostController.getAll);
-app.get('/posts/:id', PostController.getOne);
-app.get('/posts/user/:id', PostController.getUserPosts);
-app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.get("/posts/user/:id", PostController.getUserPosts);
+app.post(
+    "/posts",
+    checkAuth,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.create
+);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+    "/posts/:id",
+    checkAuth,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.update
+);
 
-app.post('/categories', CategoriesController.addCategory)
-app.get('/categories', CategoriesController.getCategories)
+app.post("/categories", CategoriesController.addCategory);
+app.get("/categories", CategoriesController.getCategories);
 
 app.listen(4444, (err) => {
-  if (err) {
-    return console.log(err);
-  }
+    if (err) {
+        return console.log(err);
+    }
 
-  console.log("Server OK");
+    console.log("Server OK");
 });

09415889c8141f7e9edd3a6429837f4a491700c0
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-07-06 17:51:32 +0600

    get user data

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,150 +1,157 @@
 import PostModel from "../models/Post.js";
 
 export const create = async (req, res) => {
-  try {
-    const doc = new PostModel({
-      title: req.body.title,
-      text: req.body.text,
-      tags: req.body.tags,
-      imageUrl: req.body.imageUrl,
-      user: req.userId
-    })
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            tags: req.body.tags,
+            imageUrl: req.body.imageUrl,
+            user: req.userId,
+        });
 
-    const post = await doc.save()
+        const post = await doc.save();
 
-    res.status(200).json(post)
-  } catch (err) {
-    res.status(500).json({
-      message: 'Create post error'
-    })
-  }
-}
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({
+            message: "Create post error",
+        });
+    }
+};
 
 export const getAll = async (req, res) => {
-  try {
-    const reg = new RegExp(req.query._search)
+    try {
+        const reg = new RegExp(req.query._search);
 
-    let posts = await PostModel.find({
-      title: reg,
-    }).populate('user').exec()
+        let posts = await PostModel.find({
+            title: reg,
+        })
+            .populate("user")
+            .exec();
 
-    res.status(200).json(posts)
-  } catch (err) {
-    res.status(500).json({
-      message: 'Bad reuest for posts'
-    })
-  }
-}
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({
+            message: "Bad reuest for posts",
+        });
+    }
+};
 
 export const getUserPosts = async (req, res) => {
-  try {
-    let posts = await PostModel.find({
-      user: req.params.id,
-    }).populate('user').exec()
+    try {
+        let posts = await PostModel.find({
+            user: req.params.id,
+        });
 
-    res.status(200).json(posts)
-  } catch (err) {
-    res.status(500).json({
-      message: 'Bad reuest for posts'
-    })
-  }
-}
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({
+            message: "Bad reuest for posts",
+        });
+    }
+};
 
 export const getOne = async (req, res) => {
-  try {
-    const postId = req.params.id;
+    try {
+        const postId = req.params.id;
 
-    PostModel.findOneAndUpdate(
-      {
-        _id: postId,
-      },
-      {
-        $inc: {
-          viewsCount: 1,
-        }
-      },
-      {
-        returnDocument: 'after',
-        populate: 'user'
-      }
-    ).then((doc) => {
-      if (!doc) {
-        return res.status(404).json({
-          message: 'Not found Post'
-        })
-      }
+        PostModel.findOneAndUpdate(
+            {
+                _id: postId,
+            },
+            {
+                $inc: {
+                    viewsCount: 1,
+                },
+            },
+            {
+                returnDocument: "after",
+                populate: "user",
+            }
+        )
+            .then((doc) => {
+                if (!doc) {
+                    return res.status(404).json({
+                        message: "Not found Post",
+                    });
+                }
 
-      res.json(doc)
-    }).catch(err => {
-      return res.status(500).json({
-        message: 'Can\'t return post',
-        err
-      })
-    })
-  } catch (err) {
-    res.status(500).json({
-      message: 'Bad reuest for posts',
-      err
-    })
-  }
-}
+                res.json(doc);
+            })
+            .catch((err) => {
+                return res.status(500).json({
+                    message: "Can't return post",
+                    err,
+                });
+            });
+    } catch (err) {
+        res.status(500).json({
+            message: "Bad reuest for posts",
+            err,
+        });
+    }
+};
 
 export const remove = async (req, res) => {
-  try {
-    const postId = req.params.id;
+    try {
+        const postId = req.params.id;
 
-    PostModel.findOneAndDelete({
-      _id: postId
-    }).then((doc) => {
-      if (!doc) {
-        return res.status(404).json({
-          message: 'Not found Post'
+        PostModel.findOneAndDelete({
+            _id: postId,
         })
-      }
+            .then((doc) => {
+                if (!doc) {
+                    return res.status(404).json({
+                        message: "Not found Post",
+                    });
+                }
 
-      res.json({
-        message: 'Removed'
-      })
-    }).catch((err, doc) => {
-      if (err) {
-        return res.status(500).json({
-          message: 'Can\'t delete post',
-          err
-        })
-      }
-    })
-  } catch (err) {
-    res.status(500).json({
-      message: 'Bad reuest for posts',
-      err
-    })
-  }
-}
+                res.json({
+                    message: "Removed",
+                });
+            })
+            .catch((err, doc) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: "Can't delete post",
+                        err,
+                    });
+                }
+            });
+    } catch (err) {
+        res.status(500).json({
+            message: "Bad reuest for posts",
+            err,
+        });
+    }
+};
 
 export const update = async (req, res) => {
-  try {
-    const postId = req.params.id;
+    try {
+        const postId = req.params.id;
 
-    const post = await PostModel.findByIdAndUpdate({
-        _id: postId
-      },
-      {
-        title: req.body.title,
-        text: req.body.text,
-        imageUrl: req.body.imageUrl,
-        user: req.userId,
-        tags: req.body.tags,
-      },
-      {
-        returnDocument: 'after',
-        populate: 'user'
-      })
+        const post = await PostModel.findByIdAndUpdate(
+            {
+                _id: postId,
+            },
+            {
+                title: req.body.title,
+                text: req.body.text,
+                imageUrl: req.body.imageUrl,
+                user: req.userId,
+                tags: req.body.tags,
+            },
+            {
+                returnDocument: "after",
+                populate: "user",
+            }
+        );
 
-    res.status(200).json(post)
-
-  } catch (err) {
-    return res.status(500).json({
-      message: 'Not updated'
-    })
-  }
-}
+        res.status(200).json(post);
+    } catch (err) {
+        return res.status(500).json({
+            message: "Not updated",
+        });
+    }
+};
